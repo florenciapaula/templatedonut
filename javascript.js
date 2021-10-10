396866833d7e413a6124a4713a2deebecf33b0b6
florenciapaula <89614423+florenciapaula@users.noreply.github.com>
2021-10-10 16:43:22 -0300

    Add files via upload
    .

--- javascript.js
+++ javascript.js
@@ -1,37 +1,41 @@
+function validacion(mail) {
+  var nombre = document.getElementById("nombre").value;
+  var mail = document.getElementById("mail").value;
+  var tel = document.getElementById("tel").value;
+  var subject = document.getElementById("subject").value;
+  var comentarios = document.getElementById("comentarios").value;
 
- function validacion(mail) {
+  re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-
-
-	var nombre = document.getElementById('nombre').value;
-	var mail = document.getElementById('mail').value;
-	var tel = document.getElementById('tel').value;
-	var subject = document.getElementById('subject').value;
-	var comentarios = document.getElementById('comentarios').value;
-
-
-	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-
-	if(nombre != "" && mail != "" && tel != "" && subject != "" && comentarios!= ""){
-
-	if(!re.exec(mail)){
-
-	  alert('ingrese un mail valido');
-      return (false);
-	  
-	}
-	
-	else{
-      
-	  alert('Se abrira una ventana para enviar tu mensaje');
-	  window.location.href ='mailto:mydonut@gmail.com?subject='
-                         + encodeURIComponent(subject)
-                         + "&body=" 
-                         + encodeURIComponent(
-					     "Nombre y Apellido: " + nombre + "\r" +
-						 "Mail: " + mail + "\r" +
-						 "Tel: " + tel + "\r" +
-						 "Comentarios: " + comentarios);
-		
-	}
-}}
+  if (
+    nombre != "" &&
+    mail != "" &&
+    tel != "" &&
+    subject != "" &&
+    comentarios != ""
+  ) {
+    if (!re.exec(mail)) {
+      alert("ingrese un mail valido");
+      return false;
+    } else {
+      alert("Se abrira una ventana para enviar tu mensaje");
+      window.location.href =
+        "mailto:mydonut@gmail.com?subject=" +
+        encodeURIComponent(subject) +
+        "&body=" +
+        encodeURIComponent(
+          "Nombre y Apellido: " +
+            nombre +
+            "\r" +
+            "Mail: " +
+            mail +
+            "\r" +
+            "Tel: " +
+            tel +
+            "\r" +
+            "Comentarios: " +
+            comentarios
+        );
+    }
+  }
+}
